a1479da567cb6cbc9f403b67e5a4aa7c6ed6d1cc
DuyBach2003 <bachtipch@gmail.com>
2025-09-09 09:46:36 +0700

    Add Logger to backend

--- EGS-MockToDoApp/backend/src/app.controller.ts
+++ EGS-MockToDoApp/backend/src/app.controller.ts
@@ -1,8 +1,13 @@
+/* eslint-disable @typescript-eslint/no-base-to-string */
+/* eslint-disable @typescript-eslint/restrict-template-expressions */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
 import {
   Body,
   Controller,
   Delete,
   Get,
+  Logger,
   Param,
   Patch,
   Post,
@@ -12,25 +17,30 @@ import { Task } from './task.entity';
 
 @Controller()
 export class AppController {
+  private readonly logger = new Logger(AppController.name);
   constructor(private readonly appService: AppService) {}
 
   @Get()
   findAll(): Promise<Task[]> {
+    this.logger.log(`GET / called`);
     return this.appService.findAll();
   }
   @Post()
   async create(@Body() body: { id: number; title: string }): Promise<Task[]> {
     await this.appService.create(body.id, body.title);
+    this.logger.log(`POST / called with body: ${body}`);
     return this.appService.findAll();
   }
   @Patch()
   async update(@Body() body: { id: number; title: string }): Promise<Task[]> {
     await this.appService.update(body.id, body.title);
+    this.logger.log(`PATCH / called with body: ${body}`);
     return this.appService.findAll();
   }
   @Delete(':id')
   async remove(@Param('id') id: string): Promise<Task[]> {
     await this.appService.remove(Number(id));
+    this.logger.log(`DELETE / called with id: ${id}`);
     return this.appService.findAll();
   }
 }
--- EGS-MockToDoApp/backend/src/app.service.ts
+++ EGS-MockToDoApp/backend/src/app.service.ts
@@ -1,10 +1,11 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable, Logger, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Task } from './task.entity';
 
 @Injectable()
 export class AppService {
+  private readonly logger = new Logger(AppService.name);
   constructor(
     @InjectRepository(Task)
     private taskRepository: Repository<Task>,
@@ -16,17 +17,22 @@ export class AppService {
 
   create(id: number, title: string) {
     const task = this.taskRepository.create({ id, title });
+    this.logger.log(`Task ${title} created successfully`);
     return this.taskRepository.save(task);
   }
   async update(id: number, title: string) {
     const task = await this.taskRepository.findOneBy({ id });
-    if (!task) throw new NotFoundException('Task not found');
-
+    if (!task) {
+      this.logger.error('Task not found');
+      throw new NotFoundException('Task not found');
+    }
     task.title = title;
+    this.logger.log(`Task ${title} updated successfully`);
     return this.taskRepository.save(task);
   }
 
   async remove(id: number) {
     await this.taskRepository.delete(id);
+    this.logger.log(`Task ${id} removed successfully`);
   }
 }
